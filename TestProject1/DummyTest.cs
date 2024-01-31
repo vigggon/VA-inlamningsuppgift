a030a3b2ace7d8e416bb2793e8966e4c3102c185
vigggon <viggo.asklof@gmail.com>
2024-01-31 19:10:39 +0100

    Added dummy test

---
+++ TestProject1/DummyTest.cs
@@ -0,0 +1,11 @@
+namespace TestProject1
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void TestThatAlwaysPasses()
+        {
+            Assert.True(true);
+        }
+    }
+}
